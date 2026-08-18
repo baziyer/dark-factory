248f330946ccfb4b571dc88fbcc57bc137c8951f
baziyer <109233175+baziyer@users.noreply.github.com>
2026-08-18 08:53:31 +0100

    Update lru past security advisory

--- scripts/local-ci.sh
+++ scripts/local-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-cargo +1.85.0 fmt --all -- --check
-cargo +1.85.0 clippy --locked --workspace --all-targets --all-features -- -D warnings
-cargo +1.85.0 test --locked --workspace --all-targets -- --test-threads=1
+cargo +1.86.0 fmt --all -- --check
+cargo +1.86.0 clippy --locked --workspace --all-targets --all-features -- -D warnings
+cargo +1.86.0 test --locked --workspace --all-targets -- --test-threads=1
 git diff --check
